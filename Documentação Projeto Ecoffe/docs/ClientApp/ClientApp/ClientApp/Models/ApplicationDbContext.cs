6e128663a18799f13aeeb4f89b304227b1d8bbad
Gabriel ilidio <gilidio8@gmail.com>
2021-10-19 20:53:34 -0300

    Inicial da criação do Model de Usuario.

---
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}

fdd408eff0c4adc71c5d246014d1ca359a50ab3b
Gabriel ilidio <gilidio8@gmail.com>
2021-10-19 20:53:34 -0300

    Inicial da criação do Model de Usuario.

---
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApp.Models
+{
+    [Table("USUARIO")]
+    public class Usuario
+    {
+        #region propriedades       
+        [Key] 
+        public int Id_Usuario { get; set; }
+        
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        public string Tx_Nome_Usuario { get; set; }
+
+ 
+    }
+    #endregion
+}

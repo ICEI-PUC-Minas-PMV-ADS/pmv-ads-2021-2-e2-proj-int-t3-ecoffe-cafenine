5f7ff9718070eeeef0bb2304bd61b0fdc68fca87
João Sena <joaosenadainese@gmail.com>
2021-10-20 20:27:04 -0300

    automatico ?

---
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using ClientApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public UsuarioController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Usuario Get()
+        {
+            var x = new Usuario()
+            {
+                Nm_Usuario = "teste"
+            };
+
+            return x;
+
+        }
+    }
+}

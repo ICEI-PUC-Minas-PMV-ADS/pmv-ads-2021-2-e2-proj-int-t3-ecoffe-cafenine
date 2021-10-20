039950118b1d9e323603faef32f24c7df0c18e9a
Gabriel ilidio <gilidio8@gmail.com>
2021-10-19 22:07:19 -0300

    Criação do model cartão e segurança

--- Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Models/Usuario.cs
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Models/Usuario.cs
@@ -15,9 +15,30 @@ namespace ClientApp.Models
         public int Id_Usuario { get; set; }
         
         [Required(ErrorMessage = "Este campo é obrigatorio")]
-        public string Tx_Nome_Usuario { get; set; }
+        public string Nm_Usuario { get; set; }
 
- 
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        public string Tx_Cpf { get; set; }
+        
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        public string Tx_Senha { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        public string tx_Email { get; set; }
+
+        public int Id_Endereco { get; set; }
+
+        public int Tx_Cnpj { get; set; }
+        
+        public int Id_Cartao { get; set; }
+        public Cartao Cartao { get; set; }
+
+        public int Nr_telefone { get; set; }
+
+        public bool In_Ativo { get; set; }
+
+        public bool In_Admin { get; set; }
+
     }
     #endregion
 }

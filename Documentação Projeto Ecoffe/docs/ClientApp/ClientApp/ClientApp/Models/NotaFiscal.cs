b628d86e51b2607799fa4798070aaf7f62633757
Victor Lopes <vlinvictus@gmail.com>
2021-10-19 22:03:32 -0300

    Criando model Nota Fiscal e Etapa Venda!

---
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Models/NotaFiscal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApp.Models
+{
+    [Table("NOTA_FISCAL")]
+    public class NotaFiscal
+    {
+        #region propriedades       
+        [Key]
+        public int Id_NotaFiscal { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        public string NR_NOTA_FISCAL { get; set; }
+
+        public string XML { get; set; }
+
+        public Pedido Pedido { get; set; }
+
+        public int Id_Pedido { get; set; }
+    }
+    #endregion
+}

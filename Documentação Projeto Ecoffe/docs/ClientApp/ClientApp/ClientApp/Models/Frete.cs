4962a09eb4f6f4b60b3a0c42205d3f0a4a93a398
João Sena <joaosenadainese@gmail.com>
2021-10-19 22:09:00 -0300

    model frete e pedido

---
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Models/Frete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApp.Models
+{
+    [Table("FRETE")]
+    public class Frete
+    {
+        #region propriedades       
+        [Key] 
+        public int Id_Frete { get; set; }
+        public float Vl_Frete { get; set; }
+        public string Nm_Status_Frete { get; set; }
+        public int Id_Status_Frete { get; set; }
+        public int Id_Pedido { get; set; }
+        public Pedido Pedido { get; set; }
+    }
+    #endregion
+}

46b293b5cc6df0a8bf16e919256cc12c3d6c1766
João Sena <joaosenadainese@gmail.com>
2021-10-20 21:04:45 -0300

    Controller produto

---
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using Client.App.Models;
+using ClientApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProdutoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Produto>> Get()
+        {
+            var listaProdutos = await _context.Produto.ToListAsync();
+
+            return listaProdutos;
+
+        }
+    }
+}

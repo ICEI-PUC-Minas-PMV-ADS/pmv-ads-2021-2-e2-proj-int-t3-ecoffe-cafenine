f4d4f0e20696eb769faba29cb234fc365fba9a7f
João Sena <joaosenadainese@gmail.com>
2021-10-25 21:27:09 -0300

    crud cupom

---
+++ Documentação Projeto Ecoffe/docs/ClientApp/ClientApp/ClientApp/Controllers/CupomController.cs
@@ -0,0 +1,70 @@
+﻿using Client.App.Models;
+using ClientApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CupomController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CupomController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/cupom
+        [HttpGet]
+        public async Task<IEnumerable<Cupom>> Get()
+        {
+            var listaCupom = await _context.Cupom.ToListAsync();
+
+            return listaCupom;
+        }
+
+        //GET: api/cupom/id
+        [HttpGet]
+        public async Task<Cupom> Get(int id)
+        {
+            var cupom = await _context.Cupom.FindAsync(id);
+
+            if (cupom == null)
+                throw new Exception("Cupom não encontrado.");
+
+            return cupom;
+        }
+
+        //POST: api/cupom/Cupom
+        [HttpPost]
+        public async Task<IActionResult> Create(Cupom cupom)
+        {
+            _context.Add(cupom);
+            await _context.SaveChangesAsync();
+
+            return Ok(cupom);
+        }
+
+        //PUT: api/cupom/Cupom
+        [HttpPut]
+        public async Task<IActionResult> Update(Cupom cupom)
+        {
+            var freteDb = await _context.Produto.FindAsync(cupom.Id_Cupom);
+
+            if (freteDb == null)
+                throw new Exception("Cupom não encontrado.");
+
+            _context.Update(cupom);
+            await _context.SaveChangesAsync();
+
+            return Ok(cupom);
+        }
+    }
+}

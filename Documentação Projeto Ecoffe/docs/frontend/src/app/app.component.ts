75c1e269b92c533143ecc90e7cdefd789c40ff4d
Victor Lopes <vlinvictus@gmail.com>
2021-10-28 21:18:41 -0300

    FrontEnd

---
+++ Documentação Projeto Ecoffe/docs/frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+
+
+export class AppComponent {
+ 
+  
+ 
+  nome = 'Ecoffe';
+  
+  
+}
+
---
+++ Documentação Projeto Ecoffe/docs/frontend/src/app/components/template/body/body.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHandler, HttpXhrBackend } from '@angular/common/http';
+
+
+const httpClient = new HttpClient(new HttpXhrBackend({ build: () => new XMLHttpRequest()}));
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+    produtos: any;
+
+  carregarProdutos(){
+    console.log("Inicio do Método");
+    httpClient.get<any>('https://localhost:44382/produto/GetAll').subscribe(data => {
+      console.log(data);
+    })
+    console.log(this.produtos);
+  };
+  
+  
+
+}

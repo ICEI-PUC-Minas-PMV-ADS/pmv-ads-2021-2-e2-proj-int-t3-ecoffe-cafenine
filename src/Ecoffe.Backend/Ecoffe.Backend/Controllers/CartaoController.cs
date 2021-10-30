3bde77bf03f6cf2ec195d553b6d9ec79edcee6ca
Felipe Bohm <fbvmcosta@sga.pucminas.br>
2021-10-29 23:16:25 -0300

    CRUD etapaVenda

--- src/Ecoffe.Backend/Ecoffe.Backend/Controllers/CartaoController.cs
+++ src/Ecoffe.Backend/Ecoffe.Backend/Controllers/CartaoController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace Ecoffe.Backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CartaoController : ControllerBase
     {
         private readonly ApplicationDbContext _context;

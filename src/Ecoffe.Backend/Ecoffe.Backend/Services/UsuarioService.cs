da51e80b7fd6a4b7594c374c317f905d31cb9e5f
João Sena <joaosenadainese@gmail.com>
2021-11-15 16:53:11 -0300

    excluindo controller e service do endereco, criando classe validadora de endereco

--- src/Ecoffe.Backend/Ecoffe.Backend/Services/UsuarioService.cs
+++ src/Ecoffe.Backend/Ecoffe.Backend/Services/UsuarioService.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Ecoffe.Backend.Infrastructure;
 using Ecoffe.Backend.Interfaces;
 using Ecoffe.Backend.Models;
+using Ecoffe.Backend.SharedValidators;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecoffe.Backend.Services
@@ -11,6 +12,7 @@ namespace Ecoffe.Backend.Services
     public class UsuarioService : IUsuarioService
     {
         private readonly ApplicationDbContext _context;
+        private readonly EnderecoValidator _enderecoValidator = new EnderecoValidator();
         public UsuarioService(ApplicationDbContext context)
         {
             _context = context;
@@ -35,7 +37,16 @@ namespace Ecoffe.Backend.Services
 
         private void Validate(Usuario usuario)
         {
+            if (String.IsNullOrWhiteSpace(usuario.Nome))
+                throw new Exception("Nome deve ser informado");
 
+            if (usuario.CPF == null || usuario.CPF.Length != 11 || long.TryParse(usuario.CPF, out long n) == false)
+                throw new Exception("CPF inválido");
+
+            if (String.IsNullOrWhiteSpace(usuario.Email))
+                throw new Exception("Email inválido");
+
+            _enderecoValidator.Validate(usuario.Endereco);
         }
 
     }

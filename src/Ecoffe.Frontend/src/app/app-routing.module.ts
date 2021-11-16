7dff5e4d9ee4ef8783c322734fc052ce115dba1f
João Sena <joaosenadainese@gmail.com>
2021-11-15 22:14:40 -0300

    ajuste responsividade header

--- src/Ecoffe.Frontend/src/app/app-routing.module.ts
+++ src/Ecoffe.Frontend/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './views/about/about.component';
-import { ProductsComponent } from './views/products/products.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
--- src/Ecoffe.Frontend/src/app/components/template/header/header.component.ts
+++ src/Ecoffe.Frontend/src/app/components/template/header/header.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',

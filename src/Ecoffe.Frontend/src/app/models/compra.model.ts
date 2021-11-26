f17637ee4748b879f40af7107a8bc491d1a916b5
João Sena <joaosenadainese@gmail.com>
2021-11-26 18:24:25 -0300

    models compra - front

---
+++ src/Ecoffe.Frontend/src/app/models/compra.model.ts
@@ -0,0 +1,29 @@
+import { Endereco } from './endereco.model';
+import { ProdutoCompra } from './../helpers/produtoCompra.model';
+
+export interface Compra {
+    id: number;
+    usuarioId: number;
+    dataCompra: Date;
+    statusCompra: StatusCompra; 
+    produtos: ProdutoCompra[];
+    endereco: Endereco;
+    formaPagamento: FormaPagamento;
+    valorBruto: number;
+}
+
+export enum FormaPagamento {
+    Debito = 0,
+    Credito = 1,
+    Boleto = 2,
+    PIX = 3
+}
+
+export enum StatusCompra {
+    WaitPayment = 0,
+    ConfirmedPayment = 1,
+    InPackage = 2,
+    Send = 3,
+    Delivering = 4,
+    Finished = 5
+}

9a2904daf018ba4df64dc3ba46d06f09a4eecbf5
João Sena <joaosenadainese@gmail.com>
2021-11-26 18:23:51 -0300

    models compra - back

---
+++ src/Ecoffe.Backend/Ecoffe.Backend/Enums/StatusCompra.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecoffe.Backend.Enums
+{
+    public enum StatusCompra
+    {
+        WaitPayment = 0,
+        ConfirmedPayment = 1,
+        InPackage = 2,
+        Send = 3,
+        Delivering = 4,
+        Finished = 5
+
+    }
+
+}
---
+++ src/Ecoffe.Backend/Ecoffe.Backend/Models/Compra.cs
@@ -0,0 +1,55 @@
+﻿using Ecoffe.Backend.Enums;
+using Ecoffe.Backend.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Ecoffe.Backend.Models
+{
+    [Table("Carrinho")]
+    public class Compra
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DataCompra {get; set;}
+        public StatusCompra StatusCompra { get; set; }
+        public Endereco Endereco { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+        public List<ProdutoCompra> Produtos { get; set; }
+        public decimal ValorBruto
+        {
+            get
+            {
+                if (Produtos.Count() > 0)
+                {
+                    decimal result = 0;
+
+                    foreach (var p in Produtos)
+                    {
+                        result += p.ValorTotal;
+                    }
+
+                    return result;
+                }
+                else return 0;
+            }
+            set
+            {
+                if (Produtos.Count() > 0)
+                {
+                    decimal result = 0;
+
+                    foreach (var p in Produtos)
+                    {
+                        result += p.ValorTotal;
+                    }
+
+                    ValorBruto = result;
+                }
+            }
+        }
+
+    }
+}

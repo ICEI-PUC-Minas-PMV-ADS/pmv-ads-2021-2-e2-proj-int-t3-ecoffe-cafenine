70a8e5e31f9e3ccbb61c54f74874d084591be692
Felipe Bohm <fbvmcosta@sga.pucminas.br>
2021-11-28 11:49:53 -0300

    Página Produtos em andamento
    Página Produtos em andamento, já está responsivo e pegando os produtos do Back, falta completar o front e tentar melhorar o TS.

--- src/Ecoffe.Frontend/src/app/views/products/products.component.ts
+++ src/Ecoffe.Frontend/src/app/views/products/products.component.ts
@@ -9,8 +9,10 @@ import { ProductsService } from './../../services/products.service';
 export class ProductsComponent implements OnInit {
 
   product: any;
-  y: any;
-  ultimos: any;
+  product2: any;
+  product3: any;
+  product4: any;
+  product5: any;
 
   constructor(private productService:ProductsService) { }
 
@@ -19,15 +21,41 @@ export class ProductsComponent implements OnInit {
       this.product = result;
     });
   }
-  busca(){
-    for(let x = 0; x < 16; x++){
-      if(x = 15)
-        this.getById(x);
-    }
+
+  getById2(productId: number){
+    this.productService.getById(productId).subscribe((result) =>{
+      this.product2 = result;
+    })
+  }
+
+  getById3(productId: number){
+    this.productService.getById(productId).subscribe((result) =>{
+      this.product3 = result;
+    })
+  }
+
+  getById4(productId: number){
+    this.productService.getById(productId).subscribe((result) =>{
+      this.product4 = result;
+    })
+  }
+
+  getById5(productId: number){
+    this.productService.getById(productId).subscribe((result) =>{
+      this.product5 = result;
+    })
+  }
+
+  buscaProduto(id1: number, id2: number, id3: number, id4: number, id5: number){
+    this.getById(id1);
+    this.getById2(id2);
+    this.getById3(id3);
+    this.getById4(id4);
+    this.getById5(id5);
   }
 
   ngOnInit(): void {
-    this.busca();
+    this.buscaProduto(8,15,9,10,11);
   }
 
 }
